d19cc8f0254dcb71fe1a97837de0024da22c2e72
filipjosipbionda <filip.josip.bionda@gmail.com>
2025-04-10 21:05:04 +0200

    Add server.js

--- projekt/server.js
+++ projekt/server.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const app = express();
-app.use(express.static('public')); // "posluzuje" index.html
-// Automatski koristi sve iz mape public
+const PORT = process.env.PORT || 3000;
 app.get('/', (req, res) => {
-res.send("Ili obican tekst ako nema HTML datoteke.");
+res.send('Pozdrav sa Railway servera!');
 });
-app.listen(3000, () => {
-console.log("Server pokrenut na http://localhost:3000");
+app.listen(PORT, () => {
+console.log('Server pokrenut na portu ${PORT}');
 });
